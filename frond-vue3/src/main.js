680377b1c83c1a9eaa932fd4b5af9c13d62b19a1
manuel parada <solo.trabajo.20201@gmail.com>
2022-12-31 13:47:23 -0500

    configuracion de socket.io en vue 3

--- frond-vue3/src/main.js
+++ frond-vue3/src/main.js
@@ -1,6 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import io from 'socket.io-client';
 import './assets/main.css'
 
-createApp(App).mount('#app')
+// Crea una instancia de Socket.IO
+const socket = io('http://localhost:3000/');
+
+// Escucha el evento 'message' del servidor y muestra el mensaje en la consola
+socket.on('message', message => {
+  console.log(message);
+});
+
+const app = createApp(App)
+app.mount('#app')
+
+// Agrega la instancia de Socket.IO a la aplicación Vue como una propiedad global
+app.config.globalProperties.socket = socket;
